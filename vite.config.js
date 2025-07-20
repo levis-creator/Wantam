99bc35a7e8cfd5ab47385de03a1d58928a4ffad0
levis-creator <47003569+levis-creator@users.noreply.github.com>
2025-07-20 22:45:14 +0300

    http test

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    base:'/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
